15790e6a519ae40d320aeb99e1c04c2cb25b070d
kimo741 <kimoelgamal741@gmail.com>
2022-11-05 18:02:44 +0200

    som edits

--- src/components/products/FilterBar.vue
+++ src/components/products/FilterBar.vue
@@ -1,24 +1,39 @@
 <template>
   <div class="filter-bar horizental-list">
-    <div class="my-card text-sm text-hint active">الكل</div>
-    <div class="my-card text-sm text-hint">وجبات سريعه</div>
-    <div class="my-card text-sm text-hint">مقبلات</div>
-    <div class="my-card text-sm text-hint">سندوتشات</div>
-    <div class="my-card text-sm text-hint">سندوتشات</div>
-    <div class="my-card text-sm text-hint">سندوتشات</div>
-    <div class="my-card text-sm text-hint">سندوتشات</div>
-    <div class="my-card text-sm text-hint">مشروبات</div>
+    <div
+      v-for="(item, i) in taps"
+      :key="i"
+      class="my-card text-sm text-hint"
+      :class="tap_active == item ? 'active' : ''"
+      @click="sellectTap(item)"
+    >
+      {{ item }}
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+export default {
+  setup() {
+    return {
+      tap_active: ref("الكل"),
+      taps: ref(["الكل", "وجبات سريعه", "مقبلات", "سندوتشات", "مشروبات"]),
+    };
+  },
+  methods: {
+    sellectTap(item) {
+      this.tap_active = item;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .filter-bar {
   width: 100%;
   .my-card {
+    transition: all 0.1s ease-in-out;
     position: relative;
     &::after {
       content: "";
@@ -30,16 +45,13 @@ export default {};
       height: 2px;
       background-color: rgba(0, 0, 0, 0.1);
     }
+    &:active {
+      background-color: rgba(0, 0, 0, 0.1);
+    }
   }
   .active {
     position: relative;
     &::after {
-      content: "";
-      position: absolute;
-      top: 115%;
-      right: 0;
-      left: 0;
-      height: 2px;
       background-color: rgba(0, 0, 0, 0.5);
     }
   }
